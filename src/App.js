3080295f2e496b4ed6b8333b87b04418dc3159ad
Soslan <sparksoslan@gmail.com>
2020-10-17 14:02:16 +0100

    Jumbotron restructor and styling

--- src/App.js
+++ src/App.js
@@ -7,8 +7,14 @@ export default function App() {
     <div className="App">
       <Jumbotron.Container>
         {jumboData.map((item) => (
-          <Jumbotron key={item} direction={item.direction}>
-            <p>Hello</p>
+          <Jumbotron key={item.id} direction={item.direction}>
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{item.title}</Jumbotron.Title>
+              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+            </Jumbotron.Pane>
+            <Jumbotron.Pane>
+              <Jumbotron.Image src={item.image} alt={item.alt} />
+            </Jumbotron.Pane>
           </Jumbotron>
         ))}
       </Jumbotron.Container>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
 import App from './App';
+import { GlobalStyles } from './global-styles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
+  document.getElementById('root')
+);
