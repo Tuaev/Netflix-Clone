f3d1a7c105821a340297b82dfcd69375e264e4fc
Soslan <sparksoslan@gmail.com>
2020-10-19 16:03:30 +0100

    Add Accordion FAQs map and toggle

--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -1,1 +1,9 @@
-import styles from 'styled-components/macro';
+import styled from 'styled-components/macro';
+
+export const Container = styled.div``;
+export const Item = styled.div``;
+export const Header = styled.h1``;
+export const Body = styled.div``;
+export const Inner = styled.div``;
+export const Frame = styled.div``;
+export const Title = styled.h1``;
