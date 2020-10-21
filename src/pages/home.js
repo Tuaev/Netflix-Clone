a9be2f057e1b2c5d4c30be1a6bed7cbca352a34b
Soslan <sparksoslan@gmail.com>
2020-10-21 16:12:15 +0100

    Add routes

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { JumbotronContainer } from '../containers/jumbotron';
+import { FooterContainer } from '../containers/footer';
+import { FaqsContainer } from '../containers/faqs';
+export default function Home() {
+  return (
+    <>
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
+    </>
+  );
+}
