71b79dee5f7e5dec0a711e26107fc3f1be8fb3e3
Soslan <sparksoslan@gmail.com>
2020-10-22 15:30:25 +0100

    Add Header components

---
+++ src/components/feature/styles/feature.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components/macro';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-bottom: 8px solid #222;
+  text-align: center;
+  padding: 165px 45px;
+
+  @media (max-width: 600px) {
+    padding: 40px 20px;
+  }
+`;
+
+export const Title = styled.h1`
+  color: white;
+  max-width: 640px;
+  font-size: 50px;
+  font-weight: 700;
+  margin: auto;
+
+  @media (max-width: 600px) {
+    font-size: 28px;
+  }
+`;
+
+export const SubTitle = styled.h2`
+  color: white;
+  font-size: 26px;
+  font-weight: normal;
+  margin: 16px auto;
+
+  @media (max-width: 600px) {
+    font-size: 18px;
+  }
+`;
--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -17,9 +17,9 @@ export function FaqsContainer() {
         <OptForm.Text>
           Ready to watch? Enter your email to create or restart your membership.
         </OptForm.Text>
+        <OptForm.Break />
         <OptForm.Input placeholder="Email Address" />
         <OptForm.Button>GET STARTED</OptForm.Button>
-        <OptForm.Break />
       </OptForm>
     </Accordion>
   );
