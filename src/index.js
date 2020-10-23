0d49fe66e9f07dc8028e827014829b78352565dd
Soslan <sparksoslan@gmail.com>
2020-10-23 17:31:41 +0100

    Add authentication listener

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import App from './App';
 import { GlobalStyles } from './global-styles';
-import { firebase } from './lib/firebase.prod.js';
+import { firebase } from './lib/firebase.prod';
 import { FirebaseContext } from './context/firebase';
 
 ReactDOM.render(
