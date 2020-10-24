00149153dde973f7b4a33153b211c8790e3c54c4
Soslan <sparksoslan@gmail.com>
2020-10-24 18:12:01 +0100

    Add film and series filter for browse page

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { useContent } from '../hooks';
+import selectionFilter from '../utils/selection-filter';
+import { BrowseContainer } from '../containers/browse';
+
 export default function Browse() {
   const { series } = useContent('series');
   const { films } = useContent('films');
   console.log(series);
-  console.log(films);
-  return <p>hello from browse</p>;
+  const slides = selectionFilter({ series, films });
+  // console.log(slides);
+  return <BrowseContainer slides={slides} />;
 }
