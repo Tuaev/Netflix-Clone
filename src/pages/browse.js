c324cab14f619f4785acabbd153883501c054181
Soslan <sparksoslan@gmail.com>
2020-10-24 18:19:12 +0100

    Fix selectionFilter .filter

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { useContent } from '../hooks';
-import selectionFilter from '../utils/selection-filter';
 import { BrowseContainer } from '../containers/browse';
+import { useContent } from '../hooks';
+import { selectionFilter } from '../utils';
 
 export default function Browse() {
   const { series } = useContent('series');
   const { films } = useContent('films');
-  console.log(series);
   const slides = selectionFilter({ series, films });
-  // console.log(slides);
+  console.log(slides);
   return <BrowseContainer slides={slides} />;
 }
--- src/utils/selection-filter.js
+++ src/utils/selection-filter.js
@@ -1,47 +1,47 @@
-export default function selectionFilter({ series, films }) {
+export default function selectionFilter({ series, films } = []) {
   return {
     series: [
       {
         title: 'Documentaries',
-        data: series.filter((item) => item.genre === 'documentaries'),
+        data: Object.values(series).filter((item) => item.genre === 'documentaries'),
       },
       {
         title: 'Comedies',
-        data: series.filter((item) => item.genre === 'comedies'),
+        data: Object.values(series).filter((item) => item.genre === 'comedies'),
       },
       {
         title: 'Children',
-        data: series.filter((item) => item.genre === 'children'),
+        data: Object.values(series).filter((item) => item.genre === 'children'),
       },
       {
         title: 'Crime',
-        data: series.filter((item) => item.genre === 'crime'),
+        data: Object.values(series).filter((item) => item.genre === 'crime'),
       },
       {
         title: 'Feel Good',
-        data: series.filter((item) => item.genre === 'feel-good'),
+        data: Object.values(series).filter((item) => item.genre === 'feel-good'),
       },
     ],
     films: [
       {
         title: 'Drama',
-        data: films.filter((item) => item.genre === 'drama'),
+        data: Object.values(films).filter((item) => item.genre === 'drama'),
       },
       {
         title: 'Thriller',
-        data: films.filter((item) => item.genre === 'thriller'),
+        data: Object.values(films).filter((item) => item.genre === 'thriller'),
       },
       {
         title: 'Children',
-        data: films.filter((item) => item.genre === 'children'),
+        data: Object.values(films).filter((item) => item.genre === 'children'),
       },
       {
         title: 'Suspense',
-        data: films.filter((item) => item.genre === 'suspense'),
+        data: Object.values(films).filter((item) => item.genre === 'suspense'),
       },
       {
         title: 'Romance',
-        data: films.filter((item) => item.genre === 'romance'),
+        data: Object.values(films).filter((item) => item.genre === 'romance'),
       },
     ],
   };
